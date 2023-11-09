1c4072c13a5ffe8408359d77b14e533e57b9f3b3
ToxEvo <dawsalowy@gmail.com>
2023-11-09 01:40:46 +0000

    Optimised Code for MainForm.cs & MainForm.Designer.cs

--- C3624738/MainForm.Designer.cs
+++ C3624738/MainForm.Designer.cs
@@ -6,6 +6,12 @@ partial class MainForm
     ///  Required designer variable.
     /// </summary>
     private System.ComponentModel.IContainer components = null;
+    private TextBox commandTextBox;
+    private TextBox programTextBox;
+    private Button runButton;
+    private Button syntaxCheckButton;
+    private Panel drawingPanel;
+
 
     /// <summary>
     ///  Clean up any resources being used.
@@ -32,6 +38,57 @@ partial class MainForm
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(800, 450);
         this.Text = "Form1";
+
+        commandTextBox = new TextBox
+        {
+            Location = new Point(10, 10),
+            Size = new Size(200, 40),
+            Multiline = true,
+            ScrollBars = ScrollBars.Vertical 
+        };
+        this.Controls.Add(commandTextBox);
+
+        programTextBox = new TextBox
+        {
+            Location = new Point(10, 60),
+            Size = new Size(200, 400),
+            Multiline = true,
+            ScrollBars = ScrollBars.Vertical
+        };
+        this.Controls.Add(programTextBox);
+
+        drawingPanel = new Panel
+        {
+            Location = new Point(220, 10),
+            Size = new Size(300, 300),
+            BorderStyle = BorderStyle.FixedSingle 
+        };
+        this.Controls.Add(drawingPanel);
+
+        runButton = new Button
+        {
+            Text = "Run",
+            Location = new Point(220, 10),
+            Size = new Size(80, 40)
+        };
+
+        runButton.Click += RunButton_Click;
+        Controls.Add(runButton);
+
+        syntaxCheckButton = new Button
+        {
+            Text = "Syntax",
+            Location = new Point(310, 10),
+            Size = new Size(80, 40)
+        };
+
+        syntaxCheckButton.Click += SyntaxCheckButton_Click;
+        Controls.Add(syntaxCheckButton);
+
+        AutoScaleDimensions = new SizeF(6F, 13F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Text = "C3624738 Graphics";
     }
 
     #endregion
--- C3624738/MainForm.cs
+++ C3624738/MainForm.cs
@@ -2,11 +2,6 @@ namespace C3624738
 {
     public partial class MainForm : Form
     {
-        private TextBox? commandTextBox;
-        private TextBox? programTextBox;
-        private Button? runButton;
-        private Button? syntaxCheckButton;
-        private Panel? drawingPanel;
 
         public MainForm()
         {
@@ -16,56 +11,7 @@ namespace C3624738
 
         private void InitializeCustomControls()
         {
-            commandTextBox = new TextBox
-            {
-                Location = new Point(10, 10),
-                Size = new Size(200, 40),
-                Multiline = true,
-                ScrollBars = ScrollBars.Vertical 
-            };
-            this.Controls.Add(commandTextBox);
-
-            programTextBox = new TextBox
-            {
-                Location = new Point(10, 60),
-                Size = new Size(200, 400),
-                Multiline = true,
-                ScrollBars = ScrollBars.Vertical
-            };
-            this.Controls.Add(programTextBox);
-
-            drawingPanel = new Panel
-            {
-                Location = new Point(220, 10),
-                Size = new Size(300, 300),
-                BorderStyle = BorderStyle.FixedSingle 
-            };
-            this.Controls.Add(drawingPanel);
-
-            runButton = new Button
-            {
-                Text = "Run",
-                Location = new Point(220, 10),
-                Size = new Size(80, 40)
-            };
-
-            runButton.Click += RunButton_Click;
-            Controls.Add(runButton);
-
-            syntaxCheckButton = new Button
-            {
-                Text = "Syntax",
-                Location = new Point(310, 10),
-                Size = new Size(80, 40)
-            };
-
-            syntaxCheckButton.Click += SyntaxCheckButton_Click;
-            Controls.Add(syntaxCheckButton);
-
-            AutoScaleDimensions = new SizeF(6F, 13F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "C3624738 Graphics";
+            
         }
 
         private void RunButton_Click(object? sender, EventArgs e)
