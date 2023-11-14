1a83abff04d03bc75684de4101a6615bc8789abb
ToxEvo <dawsalowy@gmail.com>
2023-11-14 01:58:38 +0000

    Completed XML for GraphicalSystem.cs

--- C3624738/GraphicalSystem.cs
+++ C3624738/GraphicalSystem.cs
@@ -16,7 +16,7 @@ namespace C3624738
         /// Retrieves the current drawing color.
         /// </summary>
         /// <returns>A tuple representing the ARGB color components.</returns>
-        (int, int, int, int) GetColor();
+        (int alpha, int red, int green, int blue) GetColor();
 
         /// <summary>
         /// Draws a circle at the specified coordinates with the given radius.
@@ -56,7 +56,7 @@ namespace C3624738
         /// Gets the current position of the graphical cursor.
         /// </summary>
         /// <returns>The current coordinates as a tuple.</returns>
-        (int, int) GetCoords();
+        (int x, int y) GetCoords();
 
         /// <summary>
         /// Sets the current position of the graphical cursor.
@@ -123,64 +123,127 @@ namespace C3624738
             }
         }
 
-        public void SetColor((int, int, int, int) color)
+        /// <summary>
+        /// Sets the color of the pen used for drawing.
+        /// </summary>
+        /// <param name="color">A tuple containing alpha, red, green, and blue color components.</param>
+        public void SetColor((int alpha, int red, int green, int blue) color)
         {
-            pen.Color = Color.FromArgb(color.Item1, color.Item2, color.Item3, color.Item4);
+            // Set the pen color using the Color.FromArgb method to convert ARGB values to a Color object.
+            pen.Color = Color.FromArgb(color.alpha, color.red, color.green, color.blue);
         }
 
-        public  (int, int, int, int) GetColor() 
+        /// <summary>
+        /// Gets the current color of the pen.
+        /// </summary>
+        /// <returns>A tuple containing alpha, red, green, and blue color components of the current pen color.</returns>
+        public (int alpha, int red, int green, int blue) GetColor() 
         {
+            // Retrieve the current color from the pen and return it as a tuple.
             Color color = pen.Color;
             return (color.A, color.R, color.G, color.B);
         }
 
+        /// <summary>
+        /// Draws a circle at the specified location with the specified radius.
+        /// </summary>
+        /// <param name="x">The x-coordinate of the circle's center.</param>
+        /// <param name="y">The y-coordinate of the circle's center.</param>
+        /// <param name="radius">The radius of the circle.</param>
         public void Circle(int x, int y, int radius)
         {
+            // Create a new Circle object and add it to the list of shapes.
             Circle circle = new Circle(pen.Color, x, y, pen.Width, fill, radius);
             shapes.Add(circle);
         }
 
+        /// <summary>
+        /// Clears all shapes from the current graphics context.
+        /// </summary>
         public void Clear()
         {
+            // Remove all shapes from the list.
             shapes.Clear();
         }
 
+        /// <summary>
+        /// Gets the current fill status for shapes.
+        /// </summary>
+        /// <returns>Whether shapes are currently being filled.</returns>
         public bool GetFill()
         {
+            // Return the current fill status.
             return fill;
         }
 
+        /// <summary>
+        /// Sets the fill status for shapes.
+        /// </summary>
+        /// <param name="fill">Whether shapes should be filled.</param>
         public void SetFill(bool fill)
         {
+            // Update the fill status.
             this.fill = fill;
         }
 
-        public (int, int) GetCoords()
+        /// <summary>
+        /// Retrieves the current coordinates of the pen.
+        /// </summary>
+        /// <returns>The current pen coordinates as a tuple.</returns>
+        public (int x, int y) GetCoords()
         {
+            // Return the current position of the pen.
             return penCoords;
         }
+
+        /// <summary>
+        /// Sets the pen coordinates to the specified location.
+        /// </summary>
+        /// <param name="x">The x-coordinate to set.</param>
+        /// <param name="y">The y-coordinate to set.</param>
         public void SetCoords(int x, int y)
         {
+            // Update the current position of the pen.
             penCoords = (x, y);
         }
 
+        /// <summary>
+        /// Moves to a new location without drawing.
+        /// </summary>
+        /// <param name="x">The x-coordinate to move to.</param>
+        /// <param name="y">The y-coordinate to move to.</param>
         public void MoveTo(int x, int y)
         {
+            // Set the current pen coordinates to the new location without drawing a line.
             penCoords = (x, y);
         }
 
+        /// <summary>
+        /// Draws a rectangle with the specified width and height at the specified location.
+        /// </summary>
+        /// <param name="x">The x-coordinate of the rectangle's top-left corner.</param>
+        /// <param name="y">The y-coordinate of the rectangle's top-left corner.</param>
+        /// <param name="width">The width of the rectangle.</param>
+        /// <param name="height">The height of the rectangle.</param>
         public void Rectangle(int x, int y, int width, int height)
         {
+            // Create a new Rectangle object and add it to the list of shapes.
             Rectangle rect = new Rectangle(pen.Color, x, y, pen.Width, fill, width, height);
             shapes.Add(rect);
         }
 
+        /// <summary>
+        /// Draws a line from the current pen position to the specified location.
+        /// </summary>
+        /// <param name="x">The x-coordinate to draw to.</param>
+        /// <param name="y">The y-coordinate to draw to.</param>
         public void DrawTo(int x, int y)
         {
-            // Draw a line from current penCoords to the new coordinates
+            // Create a new Line object representing the line from the current position to the specified coordinates and add it to the list of shapes.
             Line line = new Line(pen.Color, penCoords.x, penCoords.y, x, y, pen.Width);
             shapes.Add(line);
-            MoveTo(x, y); // Update current pen position
+            // Update the pen position after drawing the line.
+            MoveTo(x, y);
         }
     }
 }
