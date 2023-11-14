27e2ef73db5b07ca8bef9f4a635239c923f95d96
ToxEvo <dawsalowy@gmail.com>
2023-11-14 01:47:23 +0000

    Adding XML to GraphicalSystem.cs

--- C3624738/GraphicalSystem.cs
+++ C3624738/GraphicalSystem.cs
@@ -1,20 +1,88 @@
+
 namespace C3624738
 {
+    /// <summary>
+    /// Defines the graphical operations that can be performed.
+    /// </summary>
     interface IGraphical
     {
-        public void SetColor((int, int, int, int) color);
-        public (int, int, int, int) GetColor();
-        public void Circle(int x, int y, int radius);
-        public void Rectangle(int x, int y, int width, int height);
-        public void Clear();
-        public bool GetFill();
-        public void SetFill(bool fill);
-        public (int, int) GetCoords();
+        /// <summary>
+        /// Sets the current drawing color.
+        /// </summary>
+        /// <param name="color">A tuple representing the ARGB color components.</param>
+        void SetColor((int alpha, int red, int green, int blue) color);
+
+        /// <summary>
+        /// Retrieves the current drawing color.
+        /// </summary>
+        /// <returns>A tuple representing the ARGB color components.</returns>
+        (int, int, int, int) GetColor();
+
+        /// <summary>
+        /// Draws a circle at the specified coordinates with the given radius.
+        /// </summary>
+        /// <param name="x">The x-coordinate of the circle's center.</param>
+        /// <param name="y">The y-coordinate of the circle's center.</param>
+        /// <param name="radius">The radius of the circle.</param>
+        void Circle(int x, int y, int radius);
+
+        /// <summary>
+        /// Draws a rectangle at the specified coordinates with the given width and height.
+        /// </summary>
+        /// <param name="x">The x-coordinate of the rectangle's top-left corner.</param>
+        /// <param name="y">The y-coordinate of the rectangle's top-left corner.</param>
+        /// <param name="width">The width of the rectangle.</param>
+        /// <param name="height">The height of the rectangle.</param>
+        void Rectangle(int x, int y, int width, int height);
+
+        /// <summary>
+        /// Clears all shapes from the drawing area.
+        /// </summary>
+        void Clear();
+
+        /// <summary>
+        /// Retrieves the current fill status for shapes.
+        /// </summary>
+        /// <returns>True if shapes are filled, otherwise false.</returns>
+        bool GetFill();
+
+        /// <summary>
+        /// Sets the fill status for drawing shapes.
+        /// </summary>
+        /// <param name="fill">True to fill shapes, false to draw only their outlines.</param>
+        void SetFill(bool fill);
+
+        /// <summary>
+        /// Gets the current position of the graphical cursor.
+        /// </summary>
+        /// <returns>The current coordinates as a tuple.</returns>
+        (int, int) GetCoords();
+
+        /// <summary>
+        /// Sets the current position of the graphical cursor.
+        /// </summary>
+        /// <param name="x">The x-coordinate to move to.</param>
+        /// <param name="y">The y-coordinate to move to.</param>
         void SetCoords(int x, int y);
-        public void MoveTo (int x, int y);        
-        public void DrawTo(int x, int y);
+
+        /// <summary>
+        /// Moves the cursor to the specified location without drawing.
+        /// </summary>
+        /// <param name="x">The x-coordinate to move to.</param>
+        /// <param name="y">The y-coordinate to move to.</param>
+        void MoveTo(int x, int y);
+
+        /// <summary>
+        /// Draws a line to the specified coordinates from the current position.
+        /// </summary>
+        /// <param name="x">The x-coordinate to draw to.</param>
+        /// <param name="y">The y-coordinate to draw to.</param>
+        void DrawTo(int x, int y);
     }
 
+    /// <summary>
+    /// Implements IGraphical to provide drawing operations.
+    /// </summary>
     class Graphical : IGraphical
     {
         private Pen pen;
@@ -22,20 +90,30 @@ namespace C3624738
         protected bool fill;
         protected (int x, int y) penCoords;
 
+        /// <summary>
+        /// Initializes a new instance of the Graphical class with default settings.
+        /// </summary>
         public Graphical()
         {
             pen = new Pen(Color.Black, 3);
-
             shapes = new List<Shape>();
             fill = false;
             penCoords = (0, 0);
         }
 
+        /// <summary>
+        /// Finalizes the Graphical instance by disposing of pen resources.
+        /// </summary>
         ~Graphical()
         {
             pen.Dispose();
         }
 
+        /// <summary>
+        /// The event handler for painting graphical shapes to the screen.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="pea">The PaintEventArgs containing event data.</param>
         public void GraphicalGen(object? sender, PaintEventArgs pea)
         {
             Graphics g = pea.Graphics;
